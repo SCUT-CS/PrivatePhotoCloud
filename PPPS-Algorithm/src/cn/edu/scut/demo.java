09649bc9a63a5085711e6a154463bd06bfa4b86a
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-07-21 21:27:52 +0800

    转换Demo为Intellij项目

--- PPPS-Algorithm/src/cn/edu/scut/demo.java
+++ PPPS-Algorithm/src/cn/edu/scut/demo.java
@@ -9,18 +9,18 @@ import javax.imageio.ImageIO;
 
 public class demo {
 
-	private static int encrypt(int x, int r, int d) {
-		
-		int c = x - r % d;
-		c = c < 0 ? c + d : c;
-		return c;
-	}
-	/**
-     * ¶ÁÈ¡Ò»ÕÅÍ¼Æ¬µÄRGBÖµ
+    private static int encrypt(int x, int r, int d) {
+
+        int c = x - r % d;
+        c = c < 0 ? c + d : c;
+        return c;
+    }
+    /**
+     * è¯»å–ä¸€å¼ å›¾ç‰‡çš„RGBå€¼
      */
     public static int[][][] getImagePixel(String image, String image1, String image2) {
-    	
-    	Random rnd = new Random();
+
+        Random rnd = new Random();
         File file = new File(image);
         BufferedImage bi = null;
         try {
@@ -39,17 +39,17 @@ public class demo {
         int[] rgb = new int[3];
         for(int y = minY; y < height; y++) {
             for(int x = minX; x < width; x++) {
-                //»ñÈ¡°üº¬Õâ¸öÏñËØµÄÑÕÉ«ÐÅÏ¢µÄÖµ, intÐÍ
+                //èŽ·å–åŒ…å«è¿™ä¸ªåƒç´ çš„é¢œè‰²ä¿¡æ¯çš„å€¼, intåž‹
                 int pixel = bi.getRGB(x, y);
-                //´ÓpixelÖÐ»ñÈ¡rgbµÄÖµ
+                //ä»Žpixelä¸­èŽ·å–rgbçš„å€¼
                 rgb[0] = (pixel & 0xff0000) >> 16; //r
-            	rgb[1] = (pixel & 0xff00) >> 8; //g
-            	rgb[2] = (pixel & 0xff); //b
-            	int r1 = rnd.nextInt(256);
-            	int g1 = rnd.nextInt(256);
-            	int b1 = rnd.nextInt(256);
+                rgb[1] = (pixel & 0xff00) >> 8; //g
+                rgb[2] = (pixel & 0xff); //b
+                int r1 = rnd.nextInt(256);
+                int g1 = rnd.nextInt(256);
+                int b1 = rnd.nextInt(256);
 //                pixel = rgb[2] & 0xff | (rgb[1] & 0xff) << 8 | (rgb[0] & 0xff) << 16;
-            	pixel = b1 & 0xff | (g1 & 0xff) << 8 | (r1 & 0xff) << 16;
+                pixel = b1 & 0xff | (g1 & 0xff) << 8 | (r1 & 0xff) << 16;
                 bi1.setRGB(x, y, pixel);
                 int r2 = encrypt(rgb[0], r1, 256);
                 int g2 = encrypt(rgb[1], g1, 256);
@@ -58,14 +58,14 @@ public class demo {
 //                System.out.println(((r1 + r2) % 256) + ", " + ((g1 + g2) % 256) + ", " + ((b1 + b2) % 256));
                 pixel = b2 & 0xff | (g2 & 0xff) << 8 | (r2 & 0xff) << 16;
                 bi2.setRGB(x, y, pixel);
-                
+
                 //------Record-----
-                if(rgb[0] < r1) 
-                	record[0][x][y] = 1;
+                if(rgb[0] < r1)
+                    record[0][x][y] = 1;
                 if(rgb[1] < g1)
-                	record[1][x][y] = 1;
+                    record[1][x][y] = 1;
                 if(rgb[2] < b1)
-                	record[2][x][y] = 1;
+                    record[2][x][y] = 1;
             }
         }
         try {
@@ -75,27 +75,27 @@ public class demo {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
+
         String newfile = "images/zbw.jpg";
         BufferedImage bi3 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         for(int y = minY; y < height; y++) {
             for(int x = minX; x < width; x++) {
-                //»ñÈ¡°üº¬Õâ¸öÏñËØµÄÑÕÉ«ÐÅÏ¢µÄÖµ, intÐÍ
+                //èŽ·å–åŒ…å«è¿™ä¸ªåƒç´ çš„é¢œè‰²ä¿¡æ¯çš„å€¼, intåž‹
                 int pixel1 = bi1.getRGB(x, y);
-                //´ÓpixelÖÐ»ñÈ¡rgbµÄÖµ
+                //ä»Žpixelä¸­èŽ·å–rgbçš„å€¼
                 int r1 = (pixel1 & 0xff0000) >> 16; //r
-            	int g1 = (pixel1 & 0xff00) >> 8; //g
-            	int b1 = (pixel1 & 0xff); //b
-            	
-            	int pixel2 = bi2.getRGB(x, y);
-                //´ÓpixelÖÐ»ñÈ¡rgbµÄÖµ
+                int g1 = (pixel1 & 0xff00) >> 8; //g
+                int b1 = (pixel1 & 0xff); //b
+
+                int pixel2 = bi2.getRGB(x, y);
+                //ä»Žpixelä¸­èŽ·å–rgbçš„å€¼
                 int r2 = (pixel2 & 0xff0000) >> 16; //r
-            	int g2 = (pixel2 & 0xff00) >> 8; //g
-            	int b2 = (pixel2 & 0xff); //b
-            	
-            	rgb[0] = (r1 + r2) % 256;
-            	rgb[1] = (g1 + g2) % 256;
-            	rgb[2] = (b1 + b2) % 256;
+                int g2 = (pixel2 & 0xff00) >> 8; //g
+                int b2 = (pixel2 & 0xff); //b
+
+                rgb[0] = (r1 + r2) % 256;
+                rgb[1] = (g1 + g2) % 256;
+                rgb[2] = (b1 + b2) % 256;
                 int pixel = rgb[2] & 0xff | (rgb[1] & 0xff) << 8 | (rgb[0] & 0xff) << 16;
                 bi3.setRGB(x, y, pixel);
             }
@@ -106,13 +106,13 @@ public class demo {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
+
         return record;
     }
 
     public static void genThumbnail(String origin, String newfile, int size) {
-    	
-    	File file = new File(origin);
+
+        File file = new File(origin);
         BufferedImage bi = null;
         try {
             bi = ImageIO.read(file);
@@ -123,25 +123,25 @@ public class demo {
         int width = bi.getWidth();
         int height = bi.getHeight();
         BufferedImage bi1 = new BufferedImage(width/size, height/size, BufferedImage.TYPE_INT_RGB);
-        
+
         int k = 0, t = 0;
         for(int y = 0; y < height; y += size) {
             for(int x = 0; x < width; x += size) {
-            	
-            	int sumr = 0, sumg = 0, sumb = 0;
-            	for(int j = y; j < y + size; j++) {
-            		for(int i = x; i < x + size; i++) {
-            			
-            			int pixel = bi.getRGB(i, j);
-            			sumr += (pixel & 0xff0000) >> 16; //r
-                    	sumg += (pixel & 0xff00) >> 8; //g
-                    	sumb += (pixel & 0xff); //b
-            		}
-            	}
+
+                int sumr = 0, sumg = 0, sumb = 0;
+                for(int j = y; j < y + size; j++) {
+                    for(int i = x; i < x + size; i++) {
+
+                        int pixel = bi.getRGB(i, j);
+                        sumr += (pixel & 0xff0000) >> 16; //r
+                        sumg += (pixel & 0xff00) >> 8; //g
+                        sumb += (pixel & 0xff); //b
+                    }
+                }
                 sumr /= (size * size);
                 sumg /= (size * size);
                 sumb /= (size * size);
-                
+
                 int pixel1 = sumb & 0xff | (sumg & 0xff) << 8 | (sumr & 0xff) << 16;
                 bi1.setRGB(x / size, y /size, pixel1);
             }
@@ -153,28 +153,28 @@ public class demo {
             e.printStackTrace();
         }
     }
-    
+
     public static void reConstruct(String origin1, String origin2, String newfile, int[][][] record) {
-    	
-    	int[][][] ncord = new int[3][30][30];
-    	for(int x = 0; x < record[0].length; x += 15) {
-    		for(int y = 0; y < record[0][0].length; y += 15) {
-    			
-    			for(int i = x; i < x + 15; i++) {
-            		for(int j = y; j < y + 15; j++) {
-            			
-            			ncord[0][x / 15][y / 15] += record[0][i][j];
-            			ncord[1][x / 15][y / 15] += record[1][i][j];
-            			ncord[2][x / 15][y / 15] += record[2][i][j];
-            		}
-            	}
-//    			System.out.println(ncord[0][x / 15][y / 15] + " " + ncord[1][x / 15][y / 15] + 
+
+        int[][][] ncord = new int[3][30][30];
+        for(int x = 0; x < record[0].length; x += 15) {
+            for(int y = 0; y < record[0][0].length; y += 15) {
+
+                for(int i = x; i < x + 15; i++) {
+                    for(int j = y; j < y + 15; j++) {
+
+                        ncord[0][x / 15][y / 15] += record[0][i][j];
+                        ncord[1][x / 15][y / 15] += record[1][i][j];
+                        ncord[2][x / 15][y / 15] += record[2][i][j];
+                    }
+                }
+//    			System.out.println(ncord[0][x / 15][y / 15] + " " + ncord[1][x / 15][y / 15] +
 //    					" " + ncord[1][x / 15][y / 15]);
-    		}
-    	}
-    	
-    	File file1 = new File(origin1);
-    	File file2 = new File(origin2);
+            }
+        }
+
+        File file1 = new File(origin1);
+        File file2 = new File(origin2);
         BufferedImage image1 = null, image2 = null;
         try {
             image1 = ImageIO.read(file1);
@@ -185,28 +185,28 @@ public class demo {
         }
         int width = image1.getWidth();
         int height = image2.getHeight();
-        
+
         BufferedImage image3 = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        
+
         int[] rgb = new int[3];
         for(int y = 0; y < height; y++) {
             for(int x = 0; x < width; x++) {
-                //»ñÈ¡°üº¬Õâ¸öÏñËØµÄÑÕÉ«ÐÅÏ¢µÄÖµ, intÐÍ
+                //èŽ·å–åŒ…å«è¿™ä¸ªåƒç´ çš„é¢œè‰²ä¿¡æ¯çš„å€¼, intåž‹
                 int pixel1 = image1.getRGB(x, y);
-                //´ÓpixelÖÐ»ñÈ¡rgbµÄÖµ
+                //ä»Žpixelä¸­èŽ·å–rgbçš„å€¼
                 int r1 = (pixel1 & 0xff0000) >> 16; //r
-            	int g1 = (pixel1 & 0xff00) >> 8; //g
-            	int b1 = (pixel1 & 0xff); //b
-            	
-            	int pixel2 = image2.getRGB(x, y);
-                //´ÓpixelÖÐ»ñÈ¡rgbµÄÖµ
+                int g1 = (pixel1 & 0xff00) >> 8; //g
+                int b1 = (pixel1 & 0xff); //b
+
+                int pixel2 = image2.getRGB(x, y);
+                //ä»Žpixelä¸­èŽ·å–rgbçš„å€¼
                 int r2 = (pixel2 & 0xff0000) >> 16; //r
-            	int g2 = (pixel2 & 0xff00) >> 8; //g
-            	int b2 = (pixel2 & 0xff); //b
-            	
-            	rgb[0] = r1 + r2 - (ncord[0][x][y] * 255 / 225);
-            	rgb[1] = (g1 + g2) - (ncord[1][x][y] * 255 / 225);;
-            	rgb[2] = (b1 + b2) - (ncord[2][x][y] * 255 / 225);;
+                int g2 = (pixel2 & 0xff00) >> 8; //g
+                int b2 = (pixel2 & 0xff); //b
+
+                rgb[0] = r1 + r2 - (ncord[0][x][y] * 255 / 225);
+                rgb[1] = (g1 + g2) - (ncord[1][x][y] * 255 / 225);;
+                rgb[2] = (b1 + b2) - (ncord[2][x][y] * 255 / 225);;
                 int pixel = rgb[2] & 0xff | (rgb[1] & 0xff) << 8 | (rgb[0] & 0xff) << 16;
                 image3.setRGB(x, y, pixel);
             }
@@ -218,13 +218,13 @@ public class demo {
             e.printStackTrace();
         }
     }
-    
+
     public static void main(String [] args){
-    	
-    	int[][][] record = getImagePixel("images/ZHAO.jpg", "images//zbw1.jpg", "images//zbw2.jpg");
-    	genThumbnail("images/ZHAO.jpg", "images/ZHAO_thumbnail.jpg", 15);
-    	genThumbnail("images/zbw1.jpg", "images/zbw1_thumbnail.jpg", 15);
-    	genThumbnail("images/zbw2.jpg", "images/zbw2_thumbnail.jpg", 15);
-    	reConstruct("images/zbw1_thumbnail.jpg", "images/zbw2_thumbnail.jpg", "images/zbw_thumbnail.jpg", record);
+
+        int[][][] record = getImagePixel("images/ZHAO.jpg", "images//zbw1.jpg", "images//zbw2.jpg");
+        genThumbnail("images/ZHAO.jpg", "images/ZHAO_thumbnail.jpg", 15);
+        genThumbnail("images/zbw1.jpg", "images/zbw1_thumbnail.jpg", 15);
+        genThumbnail("images/zbw2.jpg", "images/zbw2_thumbnail.jpg", 15);
+        reConstruct("images/zbw1_thumbnail.jpg", "images/zbw2_thumbnail.jpg", "images/zbw_thumbnail.jpg", record);
     }
 }
---
+++ PPPS-Algorithm/src/cn/edu/scut/text.java
@@ -0,0 +1,35 @@
+package cn.edu.scut;
+
+import java.util.Random;
+
+public class text {
+
+    private static int encrypt(int x, int r, int d) {
+
+        int c = x - r % d;
+        c = c < 0 ? c + d : c;
+        return c;
+    }
+
+    public static void main(String[] args) {
+
+        Random rnd = new Random();
+
+        int sumr = 0, sumc = 0, sumx = 0;
+        int size = 400;
+        int count = 0;
+        for(int i = 0; i < size; i++) {
+
+            int r = rnd.nextInt(256);
+            sumr += r;
+            int x = rnd.nextInt(256);
+            sumx += x;
+            int c = encrypt(x, r, 255);
+//			int c = (x - r) % 255;
+            sumc += c;
+            if(x < r)
+                count++;
+        }
+        System.out.println((sumr / size + sumc / size - 255 * count / size) + " - " + (sumx / size));
+    }
+}
