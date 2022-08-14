98e92099509baf9eafaf851ffbe4d3b9a6ed909e
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-08-14 14:44:10 +0800

    更改拍照按钮图标

--- PPPS/app/src/main/java/cn/edu/scut/ppps/MainActivity.java
+++ PPPS/app/src/main/java/cn/edu/scut/ppps/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "TODO：拍照", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
