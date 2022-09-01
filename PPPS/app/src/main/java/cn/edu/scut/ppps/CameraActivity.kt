e2feca60940f07fad6424da61ead4ac1062a3c01
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-09-01 21:29:09 +0800

    完成拍照功能

--- PPPS/app/src/main/java/cn/edu/scut/ppps/CameraActivity.kt
+++ PPPS/app/src/main/java/cn/edu/scut/ppps/CameraActivity.kt
@@ -1,16 +1,26 @@
 package cn.edu.scut.ppps
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.icu.text.SimpleDateFormat
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.view.PreviewView
+import androidx.camera.video.Recording
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import cn.edu.scut.ppps.databinding.ActivityCameraBinding
-import com.google.common.util.concurrent.ListenableFuture
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+typealias LumaListener = (luma: Double) -> Unit
 
 /**
  * Camera Activity.
@@ -20,84 +30,132 @@ import com.google.common.util.concurrent.ListenableFuture
  */
 class CameraActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityCameraBinding
-    private lateinit var cameraProvider: ProcessCameraProvider
-    private var preview: Preview? = null
-    private var camera: Camera? = null
+    private lateinit var viewBinding: ActivityCameraBinding
     private var imageCapture: ImageCapture? = null
+    private var recording: Recording? = null
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityCameraBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        // TODO Check permission
-        if (!allPermissionsGranted()) {
+        viewBinding = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
             ActivityCompat.requestPermissions(
                     this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
-        if (!allPermissionsGranted()) {
-            ActivityCompat.requestPermissions(
-                    this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
-        }
-        setUpCamera(binding.previewView)
+        // Set up the listeners for take photo and video capture buttons
+        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
+        viewBinding.videoCaptureButton.setOnClickListener { captureVideo() }
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
-    private fun setUpCamera(previewView: PreviewView) {
-        val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
-                ProcessCameraProvider.getInstance(this)
+    override fun onRequestPermissionsResult(
+            requestCode: Int, permissions: Array<String>, grantResults:
+            IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                        "获取权限失败",
+                        Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.CHINA)
+                .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/PPPS")
+            }
+        }
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+                .Builder(contentResolver,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues)
+                .build()
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+                outputOptions,
+                ContextCompat.getMainExecutor(this),
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onError(exc: ImageCaptureException) {
+                        Log.e(TAG, "拍照失败: ${exc.message}", exc)
+                    }
+                    override fun
+                            onImageSaved(output: ImageCapture.OutputFileResults){
+                        val msg = "拍照成功: ${output.savedUri}"
+                        Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                        Log.d(TAG, msg)
+                    }
+                }
+        )
+    }
+
+    private fun captureVideo() {}
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
         cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            // Preview
+            val preview = Preview.Builder()
+                    .build()
+                    .also {
+                        it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
+                    }
+            imageCapture = ImageCapture.Builder().build()
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
             try {
-                cameraProvider = cameraProviderFuture.get()
-                bindPreview(cameraProvider, previewView)
-            } catch (e: Exception) {
-                e.printStackTrace()
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                        this, cameraSelector, preview, imageCapture)
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
             }
         }, ContextCompat.getMainExecutor(this))
     }
 
-    private fun bindPreview(
-            cameraProvider: ProcessCameraProvider,
-            previewView: PreviewView
-    ) {
-        //解除所有绑定，防止CameraProvider重复绑定到Lifecycle发生异常
-        cameraProvider.unbindAll()
-        preview = Preview.Builder().build()
-        imageCapture = ImageCapture.Builder()
-                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                .setTargetRotation(binding.previewView.display.rotation)
-                .build()
-        camera = cameraProvider.bindToLifecycle(
-                this,
-                CameraSelector.DEFAULT_BACK_CAMERA, preview, imageCapture
-        )
-        preview?.setSurfaceProvider(previewView.surfaceProvider)
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+                baseContext, it) == PackageManager.PERMISSION_GRANTED
     }
 
-    //进行拍照
-    private fun takePicture() {
-        imageCapture?.let { imageCapture ->
-            val mainExecutor = ContextCompat.getMainExecutor(this)
-            imageCapture.takePicture(mainExecutor, object : ImageCapture.OnImageCapturedCallback() {
-                override fun onCaptureSuccess(image: ImageProxy) {
-                    super.onCaptureSuccess(image)
-                }
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
+    }
 
-                override fun onError(exception: ImageCaptureException) {
-                    super.onError(exception)
-                }
-            })
-
-            // 让画面闪一下，营造拍照的感觉
-            // We can only change the foreground Drawable using API level 23+ API
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                // Display flash animation to indicate that photo was captured
-                binding.root.postDelayed({
-                    binding.root.foreground = ColorDrawable(Color.WHITE)
-                    binding.root.postDelayed(
-                            { binding.root.foreground = null }, 50L
-                    )
-                }, 100L)
-            }
-        }
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS =
+                mutableListOf (
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.RECORD_AUDIO
+                ).apply {
+                    if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                        add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    }
+                }.toTypedArray()
     }
 }
