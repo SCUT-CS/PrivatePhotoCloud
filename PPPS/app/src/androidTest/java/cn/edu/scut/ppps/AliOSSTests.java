577ffd7f625b76e2a0e0cb7e617c6d91a5ab9af2
FutabaRio965 <94628220+FutabaRio965@users.noreply.github.com>
2022-09-04 10:15:27 +0800

    创建test类

---
+++ PPPS/app/src/androidTest/java/cn/edu/scut/ppps/AliOSSTests.java
@@ -0,0 +1,30 @@
+package cn.edu.scut.ppps;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Environment;
+import android.util.Log;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.GrantPermissionRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class AliOSSTests {
+
+}
