b48187c855ab331003cb21c83e5ab899bd868920
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-09-05 16:45:39 +0800

    编写计算缩略图测试

--- PPPS/app/src/androidTest/java/cn/edu/scut/ppps/DecryptThumbnailTest.java
+++ PPPS/app/src/androidTest/java/cn/edu/scut/ppps/DecryptThumbnailTest.java
@@ -1,6 +1,7 @@
 package cn.edu.scut.ppps;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Environment;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -16,7 +17,12 @@ import org.junit.runner.RunWith;
 import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Decrypt Thumbnail Unit Tests
@@ -28,6 +34,7 @@ public class DecryptThumbnailTest {
     Decrypt decrypt = null;
     Field[] fields = null;
     Method[] methods = null;
+    final String fileName = "ZHAO.jpg";
 
     /**
      * Grant permissions.
@@ -44,20 +51,16 @@ public class DecryptThumbnailTest {
     @Before
     public void setUp() {
         // 构造函数参数
-        String imgPath1 = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath()
-                + File.separator + "WeiXin"
-                + File.separator + "encrypted1.png";
-        String imgPath2 = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath()
-                + File.separator + "WeiXin"
-                + File.separator + "encrypted2.png";
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        String imgPath1 = context.getCacheDir().getAbsolutePath() + File.separator + "Disk1" + File.separator + fileName + ".ori.png";
+        String imgPath2 = context.getCacheDir().getAbsolutePath() + File.separator + "Disk2" + File.separator + fileName + ".ori.png";
         // 通过反射创造Encrypt类
         Class decryptClass = null;
         try {
             decryptClass = Class.forName("cn.edu.scut.ppps.Decrypt");
             Constructor constructor = null;
             constructor = decryptClass.getConstructor(String.class, String.class, Context.class, boolean.class);
-            decrypt = (Decrypt) constructor.newInstance(imgPath1, imgPath2, appContext, true);
+            decrypt = (Decrypt) constructor.newInstance(imgPath1, imgPath2, context, true);
         } catch (Exception e) {
             e.printStackTrace();
             Assert.fail();
@@ -84,7 +87,7 @@ public class DecryptThumbnailTest {
                 } else if (field.getName().equals("imgFilePath2")) {
                     Assert.assertEquals(imgPath2, field.get(decrypt));
                 } else if (field.getName().equals("appContext")) {
-                    Assert.assertEquals(appContext, field.get(decrypt));
+                    Assert.assertEquals(context, field.get(decrypt));
                 } else if (field.getName().equals("isThumbnail")) {
                     Assert.assertEquals(true, field.get(decrypt));
                 }
@@ -137,8 +140,31 @@ public class DecryptThumbnailTest {
      * @author Cui Yuxin
      */
     @Test
-    public void decryptThumbnailTest() {
-
+    public void decryptThumbnailTest() throws Exception{
+        Bitmap img = null;
+        for (Method method : methods) {
+            if (method.getName().equals("decryptThumbnail")) {
+                method.invoke(decrypt);
+                for (Field field : fields) {
+                    if (field.getName().equals("img")) {
+                        img = (Bitmap) field.get(decrypt);
+                    }
+                }
+                break;
+            }
+        }
+        Assert.assertNotNull(img);
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        String imgPath1 = context.getCacheDir().getAbsolutePath() + File.separator + "Disk1" + File.separator + "ZHAO_thumbnail.png";
+        Bitmap expectedImg = Utils.openImg(imgPath1);
+        Assert.assertEquals(expectedImg.getWidth(), img.getWidth());
+        Assert.assertEquals(expectedImg.getHeight(), img.getHeight());
+        //Utils.saveImg(img, context.getCacheDir().getAbsolutePath() + File.separator + "Disk1" + File.separator + "111.png");
+        for (int i = 0; i < img.getWidth(); i++) {
+            for (int j = 0; j < img.getHeight(); j++) {
+                Assert.assertEquals(expectedImg.getPixel(i, j), img.getPixel(i, j));
+            }
+        }
     }
 
     /**
