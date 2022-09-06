89efae7c8ee9b3d694545019fc2ec74f9b1bb17b
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-09-06 20:20:27 +0800

    更新选择其他图片事件响应

--- PPPS/app/src/main/java/cn/edu/scut/ppps/MainActivity.java
+++ PPPS/app/src/main/java/cn/edu/scut/ppps/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
@@ -73,6 +74,23 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    /**
+     * Override onOptionsItemSelected method，implement the menu click event.
+     * @param item MenuItem
+     * @author Cui Yuxin
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == R.id.action_settings){
+            // TODO 打开其他位置的图片
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("image/*");
+            startActivityForResult(intent,2);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
