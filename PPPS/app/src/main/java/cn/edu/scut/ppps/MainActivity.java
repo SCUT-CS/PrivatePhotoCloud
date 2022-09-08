6dcf95ae835ae22117c0dde1108b3253dd95c78a
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-09-08 20:08:56 +0800

    Check code style and remove redundant code

--- PPPS/app/src/main/java/cn/edu/scut/ppps/MainActivity.java
+++ PPPS/app/src/main/java/cn/edu/scut/ppps/MainActivity.java
@@ -30,6 +30,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
+
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
@@ -42,10 +43,11 @@ import cn.edu.scut.ppps.databinding.ActivityMainBinding;
 
 /**
  * Main Activity
+ *
  * @author Cui Yuxin
  * @source https://blog.csdn.net/weixin_38322371/article/details/106312474
  */
-public class MainActivity extends WaterPermissionActivity<AlbumModel> implements AlbumCallback,View.OnClickListener {
+public class MainActivity extends WaterPermissionActivity<AlbumModel> implements AlbumCallback, View.OnClickListener {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
@@ -70,11 +72,11 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
     // 已经选择图片的集合
     private ArrayList<String> listChoosePics = new ArrayList<>();
     // 该参数负责子线程查询图片后通知主线程更新UI
-    private Handler mHandler = new Handler(){
+    private Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            if (msg.what == 0x110){
-               // mProgressDialog.dismiss();//消失对话框
+            if (msg.what == 0x110) {
+                // mProgressDialog.dismiss();//消失对话框
                 data2View();
                 initDirPopupWindow();
             }
@@ -86,17 +88,15 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.appBarMain.toolbar);
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                /* Snackbar.make(view, "拍照", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();*/
-                // TODO 获取拍照的url并进行下一步操作
+                // TODO 获取拍照的url并进行单图片操作
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this, CameraActivity.class);
                 //intent.putExtra("")
@@ -114,20 +114,12 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-        ActivityCompat.requestPermissions(
-                this, new String[]{
-                        Manifest.permission.CAMERA,
-                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                        Manifest.permission.READ_EXTERNAL_STORAGE,
-                        Manifest.permission.RECORD_AUDIO
-                }, 123);
         initWidget();
         mBottomly.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mDirPopupWindow.setAnimationStyle(R.style.dir_popupwindow_anim);
-                mDirPopupWindow.showAsDropDown(mBottomly,0,0);
+                mDirPopupWindow.showAsDropDown(mBottomly, 0, 0);
                 lightOff();
             }
         });
@@ -140,17 +132,18 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        if (item.getItemId() == R.id.action_settings){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
             // TODO 打开其他位置的图片
             Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
             intent.addCategory(Intent.CATEGORY_OPENABLE);
             intent.setType("image/*");
-            startActivityForResult(intent,2);
-        } else if (item.getItemId() == R.id.action_muti){
+            startActivityForResult(intent, 2);
+        } else if (item.getItemId() == R.id.action_muti) {
             isMutil = !isMutil;
+            listChoosePics = new ArrayList<>();
             refresh();
-        } else if (item.getItemId() == R.id.action_next){
+        } else if (item.getItemId() == R.id.action_next) {
             if (isMutil) {
                 // TODO 完成多选逻辑
             }
@@ -167,7 +160,7 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
 
     @Override
     protected AlbumModel getModelImp() {
-        return new AlbumModel(this,this);
+        return new AlbumModel(this, this);
     }
 
     @Override
@@ -177,7 +170,7 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
 
     @Override
     protected void initWidget() {
-        statusBarColor(R.color.white,false);
+        statusBarColor(R.color.white, false);
         mGridView = findViewById(R.id.mGridView);
         mBottomly = findViewById(R.id.mBottomly);
         mDirName = findViewById(R.id.mDirName);
@@ -186,8 +179,8 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
 
     @Override
     protected void initData() {
-        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-            Toast.makeText(this,"当前存储卡不可用！",Toast.LENGTH_SHORT).show();
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Toast.makeText(this, "当前存储卡不可用！", Toast.LENGTH_SHORT).show();
             return;
         }
         requestPermission(READ_EXTERNAL_STORAGE);
@@ -195,7 +188,7 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
             @Override
             public void onClick(View v) {
                 mDirPopupWindow.setAnimationStyle(R.style.dir_popupwindow_anim);
-                mDirPopupWindow.showAsDropDown(mBottomly,0,0);
+                mDirPopupWindow.showAsDropDown(mBottomly, 0, 0);
                 lightOff();
             }
         });
@@ -209,32 +202,32 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
     @Override
     protected void doSDWrite() {
         //mProgressDialog = ProgressDialog.show(this,null,"正在加载...");
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 Uri mImgUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
                 ContentResolver cr = MainActivity.this.getContentResolver();
-                Cursor cursor = cr.query(mImgUri,null,MediaStore.Images.Media.MIME_TYPE+"=? or "+MediaStore
-                        .Images.Media.MIME_TYPE+"=?",new String[]{"image/jpeg","image/png"},MediaStore
+                Cursor cursor = cr.query(mImgUri, null, MediaStore.Images.Media.MIME_TYPE + "=? or " + MediaStore
+                        .Images.Media.MIME_TYPE + "=?", new String[]{"image/jpeg", "image/png"}, MediaStore
                         .Images.Media.DATE_MODIFIED);
-                while (cursor.moveToNext()){
+                while (cursor.moveToNext()) {
                     String path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
                     File parentFile = new File(path).getParentFile();
-                    if (parentFile == null){
+                    if (parentFile == null) {
                         continue;
                     }
                     String dirPath = parentFile.getAbsolutePath();
                     FolderBean folderBean = null;
-                    if (mDirPaths.contains(dirPath)){
+                    if (mDirPaths.contains(dirPath)) {
                         continue;
-                    }else{
+                    } else {
                         //当前文件夹之前没扫描到
                         mDirPaths.add(dirPath);
                         folderBean = new FolderBean();
                         folderBean.setDir(dirPath);
                         folderBean.setFirstImgPath(path);
                     }
-                    if (parentFile.list() == null){
+                    if (parentFile.list() == null) {
                         continue;
                     }
                     int picSize = parentFile.list(new FilenameFilter() {
@@ -242,7 +235,7 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
                         public boolean accept(File dir, String filename) {
                             if (filename.endsWith(".jpg")
                                     || filename.endsWith(".jpeg")
-                                    || filename.endsWith(".png")){
+                                    || filename.endsWith(".png")) {
                                 return true;
                             }
                             return false;
@@ -250,7 +243,7 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
                     }).length;
                     folderBean.setCount(picSize);
                     mFolderBeans.add(folderBean);
-                    if (picSize > mMaxCount){
+                    if (picSize > mMaxCount) {
                         mMaxCount = picSize;
                         mCurrentDir = parentFile;
                     }
@@ -261,21 +254,21 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
         }.start();
     }
 
-    protected void data2View(){
-        if (mCurrentDir == null){
-            Toast.makeText(this,"未扫描到任何图片",Toast.LENGTH_SHORT).show();
+    protected void data2View() {
+        if (mCurrentDir == null) {
+            Toast.makeText(this, "未扫描到任何图片", Toast.LENGTH_SHORT).show();
             return;
         }
         mImgs = Arrays.asList(mCurrentDir.list());
-        mImgAdapter = new ImageAdapter(this,mImgs,mCurrentDir.getAbsolutePath());
+        mImgAdapter = new ImageAdapter(this, mImgs, mCurrentDir.getAbsolutePath());
         mImgAdapter.setMutil(isMutil);
         mGridView.setAdapter(mImgAdapter);
-        mDirCount.setText(mMaxCount+"");
+        mDirCount.setText(mMaxCount + "");
         mDirName.setText(mCurrentDir.getName());
     }
 
-    protected void initDirPopupWindow(){
-        mDirPopupWindow = new ListImageDirPopupWindow(this,mFolderBeans);
+    protected void initDirPopupWindow() {
+        mDirPopupWindow = new ListImageDirPopupWindow(this, mFolderBeans);
         mDirPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
             @Override
             public void onDismiss() {
@@ -290,30 +283,32 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
                     @Override
                     public boolean accept(File dir, String filename) {
                         if (filename.endsWith(".jpg")
-                                ||filename.endsWith(".jpeg")
-                                ||filename.endsWith(".png")){
+                                || filename.endsWith(".jpeg")
+                                || filename.endsWith(".png")) {
                             return true;
                         }
                         return false;
                     }
                 }));
-                mImgAdapter = new ImageAdapter(MainActivity.this,mImgs,mCurrentDir.getAbsolutePath());
+                mImgAdapter = new ImageAdapter(MainActivity.this, mImgs, mCurrentDir.getAbsolutePath());
                 mImgAdapter.setMutil(isMutil);
                 mGridView.setAdapter(mImgAdapter);
-                mDirCount.setText(mImgs.size()+"");
+                mDirCount.setText(mImgs.size() + "");
                 mDirName.setText(folderBean.getName());
                 mDirPopupWindow.dismiss();
             }
         });
     }
 
-    private void lightOn(){
+    private void lightOn() {
+        // TODO 修改颜色
         WindowManager.LayoutParams lp = getWindow().getAttributes();
         lp.alpha = 1.0f;
         getWindow().setAttributes(lp);
     }
 
-    private void lightOff(){
+    private void lightOff() {
+        // TODO 修改颜色
         WindowManager.LayoutParams lp = getWindow().getAttributes();
         lp.alpha = 0.3f;
         getWindow().setAttributes(lp);
@@ -324,33 +319,45 @@ public class MainActivity extends WaterPermissionActivity<AlbumModel> implements
 
     }
 
-    /** 添加图片到已选集合中 */
-    public void setPicToList(String pathPic){
+    /**
+     * 添加图片到已选集合中
+     */
+    public void setPicToList(String pathPic) {
         listChoosePics.add(pathPic);
     }
 
-    /** 从集合中删除指定图片 */
-    public void removePicFromList(String pathPic){
+    /**
+     * 从集合中删除指定图片
+     */
+    public void removePicFromList(String pathPic) {
         listChoosePics.remove(pathPic);
     }
 
-    /** 获取图片集合 */
-    public ArrayList<String> getPicList(){
+    /**
+     * 获取图片集合
+     */
+    public ArrayList<String> getPicList() {
         return listChoosePics;
     }
 
-    /** 是否多图 */
+    /**
+     * 是否多图
+     */
     public boolean isMutil() {
         return isMutil;
     }
 
-    /** 单选的处理 */
-    public void singleGet(String path){
+    /**
+     * 单选的处理
+     */
+    public void singleGet(String path) {
         // TODO 单图的处理
     }
 
-    /** 刷新界面 */
-    public void refresh(){
+    /**
+     * 刷新界面
+     */
+    public void refresh() {
         initWidget();
         initData();
     }
