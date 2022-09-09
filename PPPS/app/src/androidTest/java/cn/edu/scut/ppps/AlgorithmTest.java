0acb52850422d847f8217420b47d93b2514e5193
johnson-cc <313875761@qq.com>
2022-09-10 02:25:01 +0800

    完成加密+解密测试

--- PPPS/app/src/androidTest/java/cn/edu/scut/ppps/AlgorithmTest.java
+++ PPPS/app/src/androidTest/java/cn/edu/scut/ppps/AlgorithmTest.java
@@ -2,28 +2,40 @@ package cn.edu.scut.ppps;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Environment;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.GrantPermissionRule;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 /**
  * Decrypt Unit Tests
- * @author Feng Yucheng
+ * @author Feng Yucheng , Huang Zixi
  */
 @RunWith(AndroidJUnit4.class)
-public class AlgorithmTest{
+public class AlgorithmTest {
+    Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    //原始图片路径
+    String imgName = "jpg_medium.jpg";
+    String imgPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath()
+            + File.separator + "WeiXin"
+            + File.separator + imgName;
+    //加密后的图片路径 除了名字都已经写死了
+    String Path1 = context.getCacheDir().getAbsolutePath() + File.separator + "Disk1" + File.separator + imgName + ".ori";
+    String Path2 = context.getCacheDir().getAbsolutePath() + File.separator + "Disk2" + File.separator + imgName + ".ori";
+
+
     /**
      * Grant permissions.
      * AndroidManifest.xml中需要声明该权限，手机需要允许USB调试修改权限。
@@ -34,33 +46,40 @@ public class AlgorithmTest{
 
     /**
      * Test Algorithm Validity.
-     * @author Cui Yuxin
+     *
+     * @author Feng Yucheng , Huang Zixi
      */
     @Test
-    public void test() throws IOException {
-        // 照片路径
-        String imgPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath()
-                + File.separator + "WeiXin"
-                + File.separator + "jpg_medium.jpg";
-        // 储存路径
-        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        String cachePath = context.getCacheDir().getAbsolutePath();
-        String Path1 = cachePath + File.separator + "Disk1" + File.separator + "jpg_medium.jpg" + ".ori";
-        String Path2 = cachePath + File.separator + "Disk2" + File.separator + "jpg_medium.jpg" + ".ori";
-        Bitmap img = null;
-        Bitmap originalImg = null;
-        Encrypt encrypt = new Encrypt(imgPath,context);
+    public void test() throws Exception {
+        //加密
+        Encrypt encrypt = new Encrypt(imgPath, context);
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(5, 10, 1000, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(16));
-        Future<Bitmap[]> results = threadPoolExecutor.submit(encrypt);
-        File file1 = new File(Path1+=".jpg");
-        File file2 = new File(Path2+=".jpg");
-        Decrypt decrypt = new Decrypt(Path1,Path1,context);
-        ThreadPoolExecutor threadPoolExecutor2 = new ThreadPoolExecutor(5, 10, 1000, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(16));
-        Future<Bitmap> results2 = threadPoolExecutor2.submit(decrypt);
-        img = results.get();
-        String imgPath3 = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath()
-                + File.separator + "WeiXin"
-                + File.separator + "ZHAO.jpg";
-        originalImg = Utils.openImg(imgPath);
+        Future<Bitmap[]> resultsEncrypt = threadPoolExecutor.submit(encrypt);
+        Bitmap[] resultEncrypt = resultsEncrypt.get();
+        Assert.assertNotNull(resultEncrypt);
+        //解密
+        Decrypt decrypt = new Decrypt(Path1, Path2, context);
+        Future<Bitmap> resultsDecrypt = threadPoolExecutor.submit(decrypt);
+        Bitmap resultDecrypt = resultsDecrypt.get();
+        Bitmap originalImg = Utils.openImg(imgPath);
+        Assert.assertNotNull(resultDecrypt);
+        Assert.assertNotNull(originalImg);
+        // 正确性检查
+        for (int i = 0; i < 1000; i++) {
+            int row = (int) (Math.random() * resultDecrypt.getHeight());
+            int col = (int) (Math.random() * resultDecrypt.getWidth());
+            int originalPixel = originalImg.getPixel(col, row);
+            int pixel = resultDecrypt.getPixel(col, row);
+            Assert.assertEquals("加密算法错误！(R)row：" + row + "col:" + col,
+                    Color.red(originalPixel), Color.red(pixel));
+            Assert.assertEquals("加密算法错误！(G)row：" + row + "col:" + col,
+                    Color.green(originalPixel), Color.green(pixel));
+            Assert.assertEquals("加密算法错误！(B)row：" + row + "col:" + col,
+                    Color.blue(originalPixel), Color.blue(pixel));
+            if (resultDecrypt.hasAlpha()) {
+                Assert.assertEquals("加密算法错误！(A)row：" + row + "col:" + col,
+                        Color.alpha(originalPixel), Color.alpha(pixel));
+            }
+        }
     }
 }
