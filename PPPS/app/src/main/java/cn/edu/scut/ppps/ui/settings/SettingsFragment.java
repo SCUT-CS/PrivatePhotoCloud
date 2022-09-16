98a2b9a0904b365ca61412036c67396bc4b779e7
Aroc <85719360+liukanshan1@users.noreply.github.com>
2022-09-16 21:39:19 +0800

    状态界面文本获取

--- PPPS/app/src/main/java/cn/edu/scut/ppps/ui/settings/SettingsFragment.java
+++ PPPS/app/src/main/java/cn/edu/scut/ppps/ui/settings/SettingsFragment.java
@@ -11,22 +11,37 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import cn.edu.scut.ppps.MainActivity;
 import cn.edu.scut.ppps.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
 
     private FragmentSettingsBinding binding;
-    private int count = 1;
     private MainActivity mainActivity;
+    private SettingsViewModel settingsViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        SettingsViewModel settingsViewModel =
+        settingsViewModel =
                 new ViewModelProvider(this).get(SettingsViewModel.class);
+        settingsViewModel.setMainActivity(mainActivity);
+        settingsViewModel.setTexts();
 
         binding = FragmentSettingsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        final TextView cloudName1 = binding.cloudName1;
+        final TextView cloudProvider1 = binding.cloudProvider1;
+        final TextView cloudName2 = binding.cloudName2;
+        final TextView cloudProvider2 = binding.cloudProvider2;
+        final TextView imageNum = binding.imageNum;
+        settingsViewModel.getCloudName1().observe(getViewLifecycleOwner(), cloudName1::setText);
+        settingsViewModel.getCloudProvider1().observe(getViewLifecycleOwner(), cloudProvider1::setText);
+        settingsViewModel.getCloudName2().observe(getViewLifecycleOwner(), cloudName2::setText);
+        settingsViewModel.getCloudProvider2().observe(getViewLifecycleOwner(), cloudProvider2::setText);
+        settingsViewModel.getImageNum().observe(getViewLifecycleOwner(), imageNum::setText);
 
         return root;
     }
@@ -40,12 +55,13 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
-
+        mainActivity.turnOffFloatingButton();
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        mainActivity.turnOnFloatingButton();
     }
 }
--- PPPS/app/src/main/java/cn/edu/scut/ppps/ui/settings/SettingsViewModel.java
+++ PPPS/app/src/main/java/cn/edu/scut/ppps/ui/settings/SettingsViewModel.java
@@ -1,19 +1,90 @@
 package cn.edu.scut.ppps.ui.settings;
 
-import androidx.lifecycle.LiveData;
+import android.app.Activity;
+import android.os.Environment;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.io.File;
+import java.util.List;
+
+import cn.edu.scut.ppps.MainActivity;
+import cn.edu.scut.ppps.Utils;
+import cn.edu.scut.ppps.cloud.Tokens;
+
 public class SettingsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> cloudName1;
+    private final MutableLiveData<String> cloudProvider1;
+    private final MutableLiveData<String> cloudName2;
+    private final MutableLiveData<String> cloudProvider2;
+    private final MutableLiveData<String> imageNum;
+    private MainActivity mainActivity;
+
 
     public SettingsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is slideshow fragment");
+        cloudName1 = new MutableLiveData<>();
+        cloudProvider1 = new MutableLiveData<>();
+        cloudName2 = new MutableLiveData<>();
+        cloudProvider2 = new MutableLiveData<>();
+        imageNum = new MutableLiveData<>();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void setMainActivity(MainActivity activity) {
+        mainActivity = activity;
+    }
+
+    public void setTexts() {
+        Tokens tokens = mainActivity.getTokens();
+        String tokenName1 = mainActivity.getTokenName1();
+        String tokenName2 = mainActivity.getTokenName2();
+        if (tokenName1 == null || tokenName2 == null) {
+            cloudName1.setValue("请先配置云存储");
+            cloudProvider1.setValue("请先配置云存储");
+            cloudName2.setValue("请先配置云存储");
+            cloudProvider2.setValue("请先配置云存储");
+        } else {
+            String provider1 = tokens.getToken(tokenName1).get("type");
+            String provider2 = tokens.getToken(tokenName2).get("type");
+            if (provider1.equals("aliyun")) {
+                provider1 = "阿里云对象云存储";
+            }
+            if (provider2.equals("aliyun")) {
+                provider2 = "阿里云对象云存储";
+            }
+            cloudName1.setValue(tokenName1);
+            cloudProvider1.setValue(provider1);
+            cloudName2.setValue(tokenName2);
+            cloudProvider2.setValue(provider2);
+        }
+        try {
+            String imgPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath()
+                    + File.separator + "Thumbnail" + File.separator;
+            List<String> existFiles = Utils.getAllFile(imgPath);
+            imageNum.setValue(String.valueOf(existFiles.size()));
+        } catch (Exception e) {
+            imageNum.setValue("0");
+        }
+    }
+
+    public MutableLiveData<String> getCloudName1() {
+        return cloudName1;
+    }
+
+    public MutableLiveData<String> getCloudProvider1() {
+        return cloudProvider1;
+    }
+
+    public MutableLiveData<String> getCloudName2() {
+        return cloudName2;
+    }
+
+    public MutableLiveData<String> getCloudProvider2() {
+        return cloudProvider2;
+    }
+
+    public MutableLiveData<String> getImageNum() {
+        return imageNum;
     }
 }
