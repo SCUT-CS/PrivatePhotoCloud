f4861d43b31d3ce67b15ada345e098ed404d4d24
johnson-cc <313875761@qq.com>
2022-10-03 14:49:30 +0800

    create activity_test

---
+++ PPPS/app/src/main/java/cn/edu/scut/ppps/test.java
@@ -0,0 +1,14 @@
+package cn.edu.scut.ppps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
